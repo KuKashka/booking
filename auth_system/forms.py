01569d8d660dd9f5cf39ce4c753f75fdb02541cf
KuKashka <ostovrah@gmail.com>
2025-11-06 20:25:50 +0200

    я вырю що вчитель поможе

---
+++ auth_system/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from auth_system.models import User
+
+
+class LoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control mb-3'})
+
+
+
+class RegisterForm(UserCreationForm):
+    date_of_birth = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control mb-3'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'phone', 'date_of_birth', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control mb-3'})
+
